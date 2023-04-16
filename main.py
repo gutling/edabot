d2f410d4011fe5bc130ef1d6c32d7402670b9ddb
Gleb <asobizaru@gmail.com>
2023-04-16 18:53:58 +0900

    initial_commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('6200404821:AAEiHQQAwR2gLbORrYRT42wWu4kHyCeOrKo')
+a = 's```adasdss'
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    m = f'Доброго времени суток, {message.from_user.first_name}'
+    bot.send_message(message.chat.id, m, parse_mode='html')
+    markup = types.ReplyKeyboardMarkup()
+    start = types.KeyboardButton('записать съеденное')
+    markup.add(start)
+    bot.send_message(message.chat.id, 'Что хотите сделать?', reply_markup=markup)
+
+
+@bot.message_handler()
+def zapis(message):
+    if message.text == 'записать съеденное':
+        markup = types.ReplyKeyboardMarkup()
+        zavtrak = types.KeyboardButton('завтрак')
+        obed = types.KeyboardButton('обед')
+        ujin = types.KeyboardButton('ужин')
+        perekus = types.KeyboardButton('перекус')
+        markup.add(zavtrak, obed, ujin, perekus)
+        bot.send_message(message.chat.id, 'Хорошо, в какой прием пищи?', reply_markup=markup)
+    else:
+        bot.send_message(message.chat.id, 'Извините, я так не умею')
+
+
+bot.polling(none_stop=True)
