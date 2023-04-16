a6638476cf792b9148b2fd86d3ec53d2493a3a04
Gleb <asobizaru@gmail.com>
2023-04-16 23:34:57 +0900

    zavtrak

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import sqlite3
 
 bot = telebot.TeleBot('6200404821:AAEiHQQAwR2gLbORrYRT42wWu4kHyCeOrKo')
 
-con = sqlite3.connect('eda.db')
-cur = con.cursor()
+con = sqlite3.connect('eda.db', check_same_thread=False)
 
 
 @bot.message_handler(commands=['start'])
@@ -14,6 +13,12 @@ def start(message):
     bot.send_message(message.chat.id, m, parse_mode='html')
     markup = types.ReplyKeyboardMarkup()
     start = types.KeyboardButton('записать съеденное')
+
+    cur = con.cursor()
+    cur.execute("""INSERT INTO users
+            (name, tg_id) VALUES (?, ?)""", (message.from_user.first_name, message.from_user.id))
+    cur.close()
+
     markup.add(start)
     bot.send_message(message.chat.id, 'Что хотите сделать?', reply_markup=markup)
 
@@ -28,10 +33,15 @@ def zapis(message):
         perekus = types.KeyboardButton('перекус')
         markup.add(zavtrak, obed, ujin, perekus)
         bot.send_message(message.chat.id, 'Хорошо, в какой прием пищи?', reply_markup=markup)
-    elif message.text == 'завтрак':
-        pass
     else:
         bot.send_message(message.chat.id, 'Извините, я так не умею')
 
+
+@bot.message_handler()
+def poest(message):
+    if message.text != 'записать съеденное' and message.text in ['завтрак', 'обед', 'ужин', 'перекус']:
+        pass
+    else:
+        bot.send_message(message.chat.id, 'Извините, я так не умею')
 
 bot.polling(none_stop=True)
